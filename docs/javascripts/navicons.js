e1b550231014649ec100f00282c2947917db93d7
Mckaroff <96990880+Mckaroff@users.noreply.github.com>
2024-01-22 16:46:28 +0300

    Update navicons.js

--- docs/javascripts/navicons.js
+++ docs/javascripts/navicons.js
@@ -10,3 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+// вариант с несколькими иконками
+// document.addEventListener("DOMContentLoaded", function() {
+    // Список пунктов навигации и соответствующих им иконок
+    var navItems = [
+        { title: "Home", iconClass: "fas fa-home" },
+        { title: "About", iconClass: "fas fa-info-circle" },
+        { title: "Contact", iconClass: "fas fa-envelope" },
+        // Добавьте дополнительные пункты здесь
+    ];
+
+    // Поиск элементов навигации и добавление иконок
+    var navLinks = document.querySelectorAll('.md-nav__link');
+    navLinks.forEach(function(link) {
+        navItems.forEach(function(item) {
+            if (link.textContent.trim() === item.title) {
+                link.innerHTML = '<i class="' + item.iconClass + '"></i> ' + link.innerHTML;
+            }
+        });
+    });
+});
