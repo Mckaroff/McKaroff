3f6b5c772bb712b335ff400f2552556cc543e28a
Mckaroff <96990880+Mckaroff@users.noreply.github.com>
2024-01-22 15:38:16 +0300

    Update navicons.js

--- docs/javascripts/navicons.js
+++ docs/javascripts/navicons.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Иконка FontAwesome, которую вы хотите добавить
+    var iconHtml = '<i class="fas fa-info-circle"></i> ';
 
+    // Поиск элемента навигации по тексту
+    var navItems = document.querySelectorAll('.md-nav__link');
+    navItems.forEach(function(item) {
+        if (item.textContent.trim() === 'Features') {
+            item.innerHTML = iconHtml + item.innerHTML;
+        }
+    });
+});
